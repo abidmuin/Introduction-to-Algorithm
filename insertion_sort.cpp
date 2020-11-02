7a5824a1077db29d622e6b17e5b2ff81dd898d32
muin2473 <muin.739@gmail.com>
2020-11-03 00:24:34 +0600

    Update insertion_sort.cpp

--- insertion_sort.cpp
+++ insertion_sort.cpp
@@ -1,54 +1,55 @@
-#include <bits/stdc++.h>
-
-using namespace std;
+#include <iostream>
+#include <vector>
 
 int main()
 {
 	int vec_size = 0;
-	cout << "Vector size = ";
-	cin >> vec_size;
-	vector<int> v1, v2;
-	cout << "Vector elements: ";
+	std::cout << "Size = ";
+	std::cin >> vec_size;
+	std::vector<int> v;
+	std::cout << "Elements: ";
 	for (int i = 0; i < vec_size; i++)
 	{
 		int temp = 0;
-		cin >> temp;
-		v1.push_back(temp);
+		std::cin >> temp;
+		v.push_back(temp);
 	}
-	v2 = v1;
 
 	//Ascending order
-	for (int j = 1; j < v1.size(); j++)
+	for (int j = 1; j < v.size(); j++)
 	{
 		int key = 0, k = 0;
-		key = v1[j];
+		key = v[j];
 		k = j - 1;
 		//going left from key untill '0' index
-		while (k >= 0 && v1[k] > key)
+		while (k >= 0 && v[k] > key)
 		{
-			v1[k + 1] = v1[k];
+			v[k + 1] = v[k];
 			k--;
 		}
-		v1[k + 1] = key;
+		v[k + 1] = key;
 	}
-	for (int l = 0; l < v1.size(); l++)
-		cout << v1[l] << " ";
+	std::cout << "Ascending: ";
+	for (int l = 0; l < v.size(); l++)
+		std::cout << v[l] << " ";
+	std::cout << std::endl;
 
 	//Descending order
 	for (int m = 1; m < vec_size; m++)
 	{
-		int key = v2[m];
+		int key = v[m];
 		int n = m - 1;
 		//going left from key untill '0' index
-		while (n >= 0 && v2[n] < key)
+		while (n >= 0 && v[n] < key)
 		{
-			v2[n + 1] = v2[n];
+			v[n + 1] = v[n];
 			n--;
 		}
-		v2[n + 1] = key;
+		v[n + 1] = key;
 	}
-	for (int o = 0; o < v2.size(); o++)
-		cout << v2[o] << " ";
-
+	std::cout << "Descending: ";
+	for (int p = 0; p < v.size(); p++)
+		std::cout << v[p] << " ";
+	std::cout << std::endl;
 	return 0;
 }
